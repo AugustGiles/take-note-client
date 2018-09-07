902b649f81d03fcbc912e17afac4e4e4d859822e
github username <github email address>
2018-09-07 15:54:46 -0400

    service worker issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 import { configureStore } from './redux/configureStore'
@@ -17,4 +17,4 @@ ReactDOM.render(
   </Provider>,
     document.getElementById('root'))
 
-registerServiceWorker();
+// registerServiceWorker();
