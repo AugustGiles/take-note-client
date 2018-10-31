a25a54c7397cf5cb464d2449ec110e93ac464d9d
August Giles <augustgiles8@gmail.com>
2018-10-31 16:29:27 -0700

    added loading animation css to play with loading animations

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import '../styles/App.css'
+import '../styles/loading-animation.css'
 
 export default class Loading extends Component {
 
   render() {
     return(
       <div className='setup'>
-
       </div>
     )
   }
